08afbdf14781dd484028945ca2633f70717995cb
Yonissa Jaquez <yonissajaquez@gmail.com>
2024-03-26 07:08:21 -0400

    tarjeta experiencia

---
+++ app/src/main/java/com/example/ugo/ui/theme/component/tarjeta_experiencia/TarjetaExperienciaContent.kt
@@ -0,0 +1,16 @@
+package com.example.ugo.ui.theme.component.tarjeta_experiencia
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun TarjetaExperienciaPreview(){
+    TarjetaExperienciaContent()
+}
+
+@Composable
+fun TarjetaExperienciaContent(){
+    Text(text = "Contenido de la tarjeta")
+}
